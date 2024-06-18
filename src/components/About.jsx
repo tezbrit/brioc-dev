529bef41dee1253eacd572f8b4a9e82219b1dbf6
tezbrit <octavio0brittez@gmail.com>
2024-06-17 22:07:04 -0300

    repairing margin bottom of About and changing order of nav elements

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@
 function About () {
 
     return (
-        <section className="flex flex-col justify-center items-start text-white p-2 mb-5 pt-14">
+        <section className="flex flex-col justify-center items-start text-white p-2 pt-14">
             <h1 className="text-2xl border p-2 rounded-xl bg-zinc-900 ml-5">Sobre Mí</h1>
             <div className="text-start pt-6">
                 <p className="text-xl px-5">
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,10 +5,22 @@ function Nav ({scrollToSection, refs}) {
     return (
         <nav className='flex fixed items-center justify-center w-full h-14 border-b border-zinc-800 top-0 backdrop-blur-md'>
             <ul className="flex gap-4 font-light text-sm md:text-xl items-center justify-center">
-                <li onClick={() => scrollToSection(refs.headerRef)} className="bg-anima transition cursor-pointer hover:scale-110 select-none">Home</li>
-                <li onClick={() => scrollToSection(refs.aboutRef)} className="bg-anima transition cursor-pointer hover:scale-110 select-none">About</li>
-                <li onClick={() => scrollToSection(refs.projectsRef)} className="bg-anima transition cursor-pointer hover:scale-110 select-none">Projects</li>
-                <li className="bg-anima transition cursor-pointer hover:scale-110 select-none">Contact</li>
+                <li 
+                    onClick={() => scrollToSection(refs.headerRef)} 
+                    className="bg-anima transition cursor-pointer hover:scale-110">
+                        Home
+                </li>
+                <li 
+                    onClick={() => scrollToSection(refs.projectsRef)} 
+                    className="bg-anima transition cursor-pointer hover:scale-110">
+                        Projects
+                </li>
+                <li 
+                    onClick={() => scrollToSection(refs.aboutRef)} 
+                    className="bg-anima transition cursor-pointer hover:scale-110">
+                        About
+                </li>
+                <li className="bg-anima transition cursor-pointer hover:scale-110">Contact</li>
             </ul>
         </nav>
     )
